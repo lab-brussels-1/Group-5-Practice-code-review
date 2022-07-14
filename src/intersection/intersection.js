4800b7748707a25baa69b3523d05badea0d7bca6
Fenny Wilriani <97218974+FennyWilriani@users.noreply.github.com>
2022-07-14 17:20:15 +0200

    Update intersection.js

--- src/intersection/intersection.js
+++ src/intersection/intersection.js
@@ -1,16 +1,3 @@
-/**
- * Creates an array of values that are in both the first and the second arrays.
- *
- * Repeated values are not duplicated in the return value, and the order of result values are determined by the first array.
- *
- * **Note:** This function returns a new array, and has no side-effects.
- *
- * @param {Array} [array=[]] - The array to inspect.
- * @param {Array} [values=[]] - The values to include.
- * @returns {Array} Returns the new array of filtered values.
- */
-//= ============================ Solution ======================================
-
 export const intersection = (arr1, arr2) => {
   const setA = new Set(arr1);
   const setB = new Set(arr2);
